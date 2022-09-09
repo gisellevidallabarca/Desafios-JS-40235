6d23c0b10a347eea13014678ae8a13ae23de1863
Giselle <gvidallabarca@gmail.com>
2022-09-08 23:10:10 -0300

    Primer Entrega PF

--- Desafio2/main.js
+++ Desafio2/main.js
@@ -1,10 +1,19 @@
-let hamburguesa;
-let hamburguesaQueso = 1000;
-let hamburguesaDoble = 1500;
-let hamburguesaBacon = 1600;
+class Menu {
+    constructor (id, nombre, precio){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+
+}
+
+const hamburguesaQueso = new Menu ("1", "Hamburguesa con queso" , 1000);
+const hamburguesaDoble = new Menu ("2", "Hamburguesa doble" , 1500);
+const hamburguesaBacon = new Menu ("3", "Hamburguesa bacon", 1600);
+
+let idMenu;
 let total = 0;
 let nombre = prompt("Ingrese su nombre")
-
 
 
 function bienvenida (){
@@ -13,30 +22,39 @@ function bienvenida (){
 
 bienvenida()
 
+const menues = [hamburguesaQueso, hamburguesaDoble, hamburguesaBacon];
+
+const menuesElegidos = []
+
 function elegiHamburguesa (){
-    hamburguesa = prompt(`Elija una opcion:
-    1: Hamburguesa con queso
-    2: Hamburguesa doble
-    3: Hamburguesa bacon
-`)
-if (hamburguesa === "1") {
-    alert("Elegiste hamburguesa con queso")
-    total += hamburguesaQueso
-}else if (hamburguesa === "2"){
-    alert("Elegiste hamburguesa doble")
-    total += hamburguesaDoble
-}else if(hamburguesa === "3"){
-    alert("Elegiste hamburguesa bacon")
-    total += hamburguesaBacon
-}
+    idMenu = prompt('Elija una opcion:' + `\n` +
+    hamburguesaQueso.id + " " + hamburguesaQueso.nombre + '\n' +
+    hamburguesaDoble.id + " " + hamburguesaDoble.nombre + '\n' +
+    hamburguesaBacon.id + " " + hamburguesaBacon.nombre  
+    )
+ 
+    cargarHamburguesa();    
+ 
 opcion = prompt(`Vuelve a ingresar una opcion: 
 2: agregar mas productos
 3: finalizar compra
 `)
-
 }
 
 
+function cargarHamburguesa(){
+    const menuBuscado = menues.find(menu => menu.id === idMenu)
+    total += menuBuscado.precio;
+    menuesElegidos.push(menuBuscado.nombre)
+    alert("Elegiste" +" " + menuBuscado.nombre)
+    /*for(let menu of menues){
+        if (idMenu === menu.id){
+            alert("Elegiste" +" " + menu.nombre)
+            total += menu.precio
+            menuesElegidos.push(menu.nombre)
+        }
+    } */  
+}
 
 function agregarProductos (){
   
@@ -50,7 +68,9 @@ function agregarProductos (){
 
 function finalizarCompra (){
         if(opcion == 3){
-            alert ("Tenes que abonar" + " " + total)
+            alert("Usted compro:" + " " + menuesElegidos.join(", ") +'\n' +
+            'Debera abonar:' + " " + "$" + total)
+           // alert ("Tiene que abonar" + " " + total)
         }
     }
 
